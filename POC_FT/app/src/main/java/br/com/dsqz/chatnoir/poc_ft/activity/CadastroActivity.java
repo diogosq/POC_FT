6dee1bc8c6254cdf483e1e8246a96bae79d9e1d3
Diogo Queiroz <diogosq@gmail.com>
2016-01-28 00:51:18 -0200

    DONE Layout de cadastro

--- POC_FT/app/src/main/java/br/com/dsqz/chatnoir/poc_ft/activity/CadastroActivity.java
+++ POC_FT/app/src/main/java/br/com/dsqz/chatnoir/poc_ft/activity/CadastroActivity.java
@@ -2,7 +2,11 @@ package br.com.dsqz.chatnoir.poc_ft.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import br.com.dsqz.chatnoir.poc_ft.R;
 import br.com.jansenfelipe.androidmask.MaskEditTextChangedListener;
@@ -13,6 +17,9 @@ public class CadastroActivity extends Activity{
     private EditText mEditTextTelefone;
     private EditText mEditTextCelular;
     private EditText mEditTextNascimento;
+    private EditText mEditTextNonActive5;
+    private EditText mEditTextNonActive6;
+    private Spinner  mSpinnerSexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -23,10 +30,44 @@ public class CadastroActivity extends Activity{
         mEditTextTelefone = (EditText) findViewById(R.id.editText_telefone);
         mEditTextCelular = (EditText) findViewById(R.id.editText_celular);
         mEditTextNascimento = (EditText) findViewById(R.id.editText_nascimento);
+        mEditTextNonActive5 = (EditText) findViewById(R.id.non_active_edit_text5);
+        mEditTextNonActive6 = (EditText) findViewById(R.id.non_active_edit_text6);
+        mSpinnerSexo = (Spinner) findViewById(R.id.spinner_sexo);
 
+        setMasksToFields();
+        initializeSpinner();
+    }
+
+    private void setMasksToFields(){
         mEditTextCPF.addTextChangedListener(new MaskEditTextChangedListener("###.###.###-##", mEditTextCPF));
         mEditTextTelefone.addTextChangedListener(new MaskEditTextChangedListener("(##) ####-####", mEditTextTelefone));
         mEditTextCelular.addTextChangedListener(new MaskEditTextChangedListener("(##) #####-####", mEditTextCelular));
         mEditTextNascimento.addTextChangedListener(new MaskEditTextChangedListener("##/##/####", mEditTextNascimento));
+    }
+
+    private void initializeSpinner(){
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this, R.array.sexo_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerSexo.setAdapter(adapter);
+
+        mSpinnerSexo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                if(position == 0){
+                    mEditTextNonActive5.setVisibility(View.VISIBLE);
+                    mEditTextNonActive6.setVisibility(View.VISIBLE);
+                }else{
+                    mEditTextNonActive5.setVisibility(View.INVISIBLE);
+                    mEditTextNonActive6.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+                mEditTextNonActive5.setVisibility(View.VISIBLE);
+                mEditTextNonActive6.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
